f4bd216d9cb6f44b5424c7750fc874e3db959eca
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-03-16 20:25:17 -0700

    update

--- hedgehog.py
+++ hedgehog.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from einops import rearrange
-from fla.ops.linear_attn import fused_chunk_linear_attn
 
 
 def quadratic_linear_attention(q: torch.Tensor, k: torch.Tensor):
@@ -62,3 +61,115 @@ class HedgehogAttention(nn.Module):
 
         if output_attentions:
             return outputs, (pred_attns, true_attns)
+
+class LinearAttention(nn.Module):
+    def __init__(
+        self,
+        d_model: str = 1024,
+        expand_k: int = 1.0,
+        expand_v: int = 1.0,
+        num_heads: int = 8,
+        mode: str = 'chunk',
+        feature_map: str = 'elementwise_product',
+        tie_feature_map_qk: bool = False,
+        output_norm: str = 'rmsnorm',
+        norm_q: bool = False,
+        norm_k: bool = False,
+        # standard linear attention normalization
+        do_feature_map_norm: bool = False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__()
+        assert feature_map in ['elu', 'relu', 'hedgehog', 't2r', 'dpfp',
+                               'identity', 'elementwise_product'], f"Not supported feature map `{feature_map}`."
+
+        assert output_norm in ['rmsnorm', 'identity'], f"Not supported output norm `{output_norm}`."
+
+        self.d_model = d_model
+        self.mode = mode
+        self.key_dim = int(d_model * expand_k)
+        self.value_dim = int(d_model * expand_v)
+        self.num_heads = num_heads
+
+        assert mode in ['chunk', 'fused_chunk', 'fused_recurrent'], f"Not suppoerted mode `{mode}`."
+        assert self.key_dim % num_heads == 0, f"key dim must be divisible by num_heads of {num_heads}"
+        assert self.value_dim % num_heads == 0, f"value dim must be divisible by num_heads of {num_heads}"
+
+        self.head_qk_dim = self.key_dim // num_heads
+        self.head_v_dim = self.value_dim // num_heads
+
+        if feature_map == 'hedgehog':
+            if tie_feature_map_qk:
+                self.feature_map_q = self.feature_map_k = HedgehogFeatureMap(head_dim=self.head_qk_dim)
+            else:
+                self.feature_map_q = HedgehogFeatureMap(head_dim=self.head_qk_dim)
+                self.feature_map_k = HedgehogFeatureMap(head_dim=self.head_qk_dim)
+
+        elif feature_map == 't2r':
+            if tie_feature_map_qk:
+                self.feature_map_q = self.feature_map_k = T2RFeatureMap(head_dim=self.head_qk_dim)
+            else:
+                self.feature_map_q = T2RFeatureMap(head_dim=self.head_qk_dim)
+                self.feature_map_k = T2RFeatureMap(head_dim=self.head_qk_dim)
+
+        elif feature_map == 'elementwise_product':
+            if tie_feature_map_qk:
+                self.feature_map_q = self.feature_map_k = HadamardFeatureMap(head_dim=self.head_qk_dim)
+            else:
+                self.feature_map_q = HadamardFeatureMap(head_dim=self.head_qk_dim)
+                self.feature_map_k = HadamardFeatureMap(head_dim=self.head_qk_dim)
+
+        elif feature_map == 'dpfp':
+            self.feature_map_q = DPFPFeatureMap(head_dim=self.head_qk_dim)
+            self.feature_map_k = DPFPFeatureMap(head_dim=self.head_qk_dim)
+
+        elif feature_map == 'elu':
+            def elu(x):
+                return F.elu(x) + 1
+            self.feature_map_q = elu
+            self.feature_map_k = elu
+
+        elif feature_map == 'relu':
+            self.feature_map_q = nn.ReLU()
+            self.feature_map_k = nn.ReLU()
+
+        elif feature_map == 'identity':
+            self.feature_map_q = nn.Identity()
+            self.feature_map_k = nn.Identity()
+        else:
+            raise NotImplementedError
+
+        self.do_feature_map_norm = do_feature_map_norm
+        if output_norm == 'rmsnorm':
+            self.norm = RMSNorm(self.head_v_dim)
+        elif output_norm == 'identity':
+            self.norm = nn.Identity()
+        else:
+            raise NotImplementedError
+
+        self.q_proj = nn.Linear(d_model, self.key_dim, bias=False)
+        self.k_proj = nn.Linear(d_model, self.key_dim, bias=False)
+        self.v_proj = nn.Linear(d_model, self.value_dim, bias=False)
+        self.o_proj = nn.Linear(self.value_dim, d_model, bias=False)
+
+        self.norm_q = norm_q
+        self.norm_k = norm_k
+
+    def forward(self, x):
+        mode = self.mode
+        q = rearrange(self.q_proj(x), 'b n (h d) -> b h n d', h=self.num_heads)
+        k = rearrange(self.k_proj(x), 'b n (h d) -> b h n d', h=self.num_heads)
+        v = rearrange(self.v_proj(x), 'b n (h d) -> b h n d', h=self.num_heads)
+        q = self.feature_map_q(q)
+        k = self.feature_map_k(k)
+        if self.norm_q:
+            q = q / (q.sum(-1, keepdim=True) + 1e-4)
+        if self.norm_k:
+            k = k / (k.sum(-1, keepdim=True) + 1e-4)
+
+        o = fused_chunk_linear_attn(q, k, v, normalize=self.do_feature_map_norm)
+        o = self.norm(o)
+        o = rearrange(o, 'b h n d -> b n (h d)')
+        o = self.o_proj(o)
+        return o
--- train.py
+++ train.py
@@ -1,27 +1,54 @@
-import torch
-from torch import nn
+from copy import deepcopy
 from hedgehog import HedgehogAttention
+from torch import nn
 from transformers import AutoModel
+from transformers.models.bart.modeling_bart import BartSdpaAttention
+import torch
+import transformers
 
-hf_name = "pszemraj/led-base-book-summary"
+hf_name = "sshleifer/distilbart-cnn-6-6"
 
 base_model = AutoModel.from_pretrained(hf_name)
 
-for p in base_model.layers:
-    base_model.attn = HedgehogAttention(base_model.attn)
+print(base_model)
+
+
+def replace_layer(module):
+    if isinstance(module, BartSdpaAttention):
+        target_state_dict = deepcopy(module.state_dict())
+        new_module = HedgehogAttention(module)
+        new_module.load_state_dict(target_state_dict)
+        return new_module
+    else:
+        return module
+
+
+def recursive_setattr(obj, attr, value):
+    attr = attr.split(".", 1)
+    if len(attr) == 1:
+        setattr(obj, attr[0], value)
+    else:
+        recursive_setattr(getattr(obj, attr[0]), attr[1], value)
+
+
+for name, module in tuple(base_model.named_modules()):
+    if name:
+        recursive_setattr(base_model, name, replace_layer(module))
+
+print(base_model)
 
 optim = torch.optim.AdamW(base_model.parameters())
 
 loss_fn = nn.CrossEntropyLoss()
 
-for data in dataloader:
-    outputs = base_model(**data, output_attentions=True)
-    outputs = outputs.get("attentions")
+# for data in dataloader:
+#     outputs = base_model(**data, output_attentions=True)
+#     outputs = outputs.get("attentions")
 
-    loss = torch.tensor(0)
-    for attns in enumerate(outputs):
-        pred_attn, true_attn = attns
-        loss += loss_fn(pred_attn, true_attn)
+#     loss = torch.tensor(0)
+#     for attns in enumerate(outputs):
+#         pred_attn, true_attn = attns
+#         loss += loss_fn(pred_attn, true_attn)
 
-    loss.backward()
-    optim.step()
+#     loss.backward()
+#     optim.step()
